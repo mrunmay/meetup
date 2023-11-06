1ce5d971afdcd41e2aa83187edcab623b6f145c5
Mrunmaya Mohanty <mrunmaya.biet@gmail.com>
2023-11-06 11:47:25 +0200

    removed unused annotation

--- src/main/java/ae/mashreq/internal/meetup/service/BookingServiceImpl.java
+++ src/main/java/ae/mashreq/internal/meetup/service/BookingServiceImpl.java
@@ -7,7 +7,6 @@ import ae.mashreq.internal.meetup.model.MaintenanceTime;
 import ae.mashreq.internal.meetup.repository.BookingRepository;
 import ae.mashreq.internal.meetup.repository.ConferenceRoomRepository;
 import ae.mashreq.internal.meetup.repository.MaintenanceTimeRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 public class BookingServiceImpl implements BookingService {
 
     @Autowired
